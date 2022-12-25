4ed5bf0080f35cb75d8a6e52ff3874e0805daf1c
Bonee94 <bonee94@yahoo.com>
2022-12-25 15:49:31 -0600

    Get for index.html and notes.html added

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const notes = require('./notes');
+
+app.use('/notes', notes)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+module.exports = app;
